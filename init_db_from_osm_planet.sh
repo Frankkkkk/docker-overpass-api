7cf0b05d4090b4c8816333d0e6210d40355669b5
Frank Villaro-Dixon <frank@vi-di.fr>
2017-11-12 19:15:17 +0100

    some logs when starting up

--- init_db_from_osm_planet.sh
+++ init_db_from_osm_planet.sh
@@ -11,10 +11,14 @@ if [ ! -e $DBDIR/nodes.bin ]; then
 	wget -O /dev/shm/state.txt "http://ftp.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/replication/minute/state.txt"
 
 	#Download the planet
-	wget -O planet.osm.bz2 $PLANET_FILE
+	echo "Will start downloading the planet file at $(date)"
+	wget --quiet -O planet.osm.bz2 $PLANET_FILE
+	echo "Planet file downloaded at $(date)"
 
 	#init the planet
+	echo "Will init the import of the planet. Can take ~40hours. Started at $(date)"
 	$BINDIR/init_osm3s.sh $DBDIR/planet.osm.bz2 $DBDIR $EXECDIR --meta
+	echo "Import finished at $(date)"
 
 	#Get 7 days of backwards history; we never know when was the planet.osm exported..
 	NUM=`cat /dev/shm/state.txt | grep sequenceNumber | cut -d'=' -f2`
